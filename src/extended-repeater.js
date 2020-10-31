e5fabdbcf72a9f24b2b48ba382331cc9c67c798e
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-11-01 02:03:34 +0300

    feat: add solution to test extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = str != null ? str.toString() : str === null ? "null" : "",
+    repeatTimes =
+      typeof options.repeatTimes == "number" ? options.repeatTimes : 1,
+    separator = options.separator ? options.separator : "+",
+    addition =
+      options.addition != null
+        ? options.addition.toString()
+        : options.addition === null
+        ? "null"
+        : "",
+    additionRepeatTimes =
+      typeof options.additionRepeatTimes == "number"
+        ? options.additionRepeatTimes
+        : 1,
+    additionSeparator = options.additionSeparator,
+    result = "";
+  for (let i = 0; i < repeatTimes; i++) {
+    result += `${string}`;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += `${addition}${
+        j == additionRepeatTimes - 1 ? "" : additionSeparator
+      }`;
+    }
+    result += `${i == repeatTimes - 1 ? "" : separator}`;
+  }
+  return result;
 };
   
