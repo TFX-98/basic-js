2992e42417a6deaeda4966ecd1987a5ab4af0259
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-11-01 02:04:45 +0300

    feat: add solution to test transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  let result = [];
+
+  arr.forEach((item) => {
+    result.push(item);
+  });
+
+  result.forEach((element, index) => {
+    element == "--discard-prev" && index == 0
+      ? result.splice(index, 1, "--delete--")
+      : element == "--discard-prev" && index != 0
+      ? result.splice(index - 1, 2, "--delete--")
+      : result;
+    element == "--discard-next" && index == result.length - 1
+      ? result.splice(index, 1, "--delete--")
+      : element == "--discard-next" && index != result.length - 1
+      ? result.splice(index, 2, "--delete--")
+      : result;
+    element == "--double-prev" && index == 0
+      ? result.splice(index, 1, "--delete--")
+      : element == "--double-prev" && index != 0
+      ? result.splice(index, 1, result[index - 1])
+      : result;
+    element == "--double-next" && index == result.length - 1
+      ? result.splice(index, 1, "--delete--")
+      : element == "--double-next" && index != result.length - 1
+      ? result.splice(index, 1, result[index + 1])
+      : result;
+  });
+
+  const final = result.filter((element) => element != "--delete--");
+  return final;
 };
