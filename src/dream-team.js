f1f59e3c0b81931069cffeddc9a9a0538525efc8
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-11-01 02:03:09 +0300

    feat: add solution to test dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secret = "";
+  if (Array.isArray(members)) {
+    members.forEach((name) => {
+      if (typeof name == "string") {
+        secret += name.replace(/\s/g, '')[0];
+      }
+	});
+    return secret.toUpperCase().split("").sort().join("");
+  } else {
+	  return false;
+  }
 };
